99ad58ca14ca248b297dab321ec64cca4cfdf5f3
DJFulton09 <121510708+DJFulton09@users.noreply.github.com>
2023-02-09 10:21:33 -0800

    made changes

---
+++ Basic_javascript_projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("Hello, World!")
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+    Daughter = "Penny", Son = "Zorro";
+document.write(Dad ); 
+
+function My_First_Function() {                  //Defining a function and naming it
+    var str = "This is the button text!";       //Defining a variable and giving it
+                                                //a string value
+    document.getElementById("Button_Text").innerHTML = str; //putting the value
+                                                //of the variable into the HTML elementFromPoint
+                                                //with the "Button_Text" id
+}
+
+var A = " this is the beginning of the string" //created a variable for Sent1
+var B = " and this is the end of the string" //created a variable for Sent2
+
+document.write(A+B) //wrote an expression to concatenate Sent1 and Sent2??
