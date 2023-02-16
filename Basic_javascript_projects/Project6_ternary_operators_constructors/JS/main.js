48c4be1522e77eb313bb585a7941b684843d55b9
DJFulton09 <121510708+DJFulton09@users.noreply.github.com>
2023-02-15 20:02:50 -0800

    made changes

--- Basic_javascript_projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic_javascript_projects/Project6_ternary_operators_constructors/JS/main.js
@@ -4,3 +4,10 @@ function Ride_Function() {
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML= Can_ride + "to ride";
 }
+
+function vote_Function() {                          //created a function
+    var Age, Can_Vote;                              //defined variables
+    Age = document.getElementById("Age").value;
+    Can_Vote = (Age < 18) ? "You are not old enough ":"You can vote! ";
+    document.getElementById("Vote").innerHTML= Can_Vote ;
+}
